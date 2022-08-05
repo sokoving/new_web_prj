aff4558d109b7028166ccdb6b3b90964150f7f67
sokoving <sokoving@gmail.com>
2022-08-05 11:13:07 +0900

    카카오 로그인 액세스 토큰 받기

---
+++ src/main/java/com/project/web_prj/member/controller/KakaoController.java
@@ -0,0 +1,39 @@
+package com.project.web_prj.member.controller;
+
+import com.project.web_prj.member.domain.OAuthValue;
+import com.project.web_prj.member.service.KakaoService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import static com.project.web_prj.member.domain.OAuthValue.*;
+
+@Controller
+@Log4j2
+@RequiredArgsConstructor
+public class KakaoController {
+
+    final KakaoService kakaoService;
+
+    @GetMapping("/kakao-test")
+    public void kakaoTest(Model model){
+        log.info("forwarding kakao-test.jsp");
+        model.addAttribute("appKey", KAKAO_APP_KEY);
+        model.addAttribute("redirectUri", KAKAO_REDIRECT_URI);
+    }
+
+    // 카카오 인증서버가 보내준 인가코드를 받아서 처리할 메서드
+    @GetMapping(KAKAO_REDIRECT_URI)
+    public String kakaoLogin(String code) throws Exception {
+        log.info("{} GET!! code - {}", KAKAO_REDIRECT_URI, code);
+
+        // 인가코드를 통해 액세스 토큰 발급받기
+        // 우리 서버에서 카카오 서버로 통신해야 함(Server to Server)
+        String accessToken = kakaoService.getAccessToken(code);
+        log.info("accessToken - {}", accessToken);
+
+        return "";
+    }
+}
---
+++ src/main/java/com/project/web_prj/member/domain/OAuthValue.java
@@ -0,0 +1,8 @@
+package com.project.web_prj.member.domain;
+
+public interface OAuthValue {
+
+    // 인터페이스 필드  > 상수
+    String KAKAO_APP_KEY = "8ffe7711553097da45ac141d7bd20552";
+    String KAKAO_REDIRECT_URI = "/oauth/kakao";
+}
